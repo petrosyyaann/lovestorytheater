2fd6bc7e7f6c537c56795f308b6301df6285d4cb
Vera Petrosyan <petrosyan.vera.2002@gmail.com>
2023-12-31 04:49:24 +0300

    Update Tickets.tsx

--- src/modules/Tickets/Tickets.tsx
+++ src/modules/Tickets/Tickets.tsx
@@ -46,18 +46,18 @@ export const Tickets = () => {
         </Flex>
 
         <Flex direction='column' p={['40px', '60px', '80px', '60px', '80px']}>
-          {/* <Image zIndex={2} src={Ticket} />
+          <Image zIndex={2} src={Ticket} />
           <Image
             mt={['-13px', '-22px', '-23px', '-24px', '-25px']}
             transform='rotate(12deg)'
             src={Ticket}
-          /> */}
-          <object style={{ zIndex: 2 }} type='image/svg+xml' data={Ticket} />
+          />
+          {/* <object style={{ zIndex: 2 }} type='image/svg+xml' data={Ticket} />
           <object
-            style={{ transform: 'rotate(12deg)' }}
+            style={{ transform: 'rotate(12deg)', marginTop: '-23px' }}
             type='image/svg+xml'
             data={Ticket}
-          />
+          /> */}
         </Flex>
       </Flex>
       <Flex py='50px' w='100%' justifyContent='center'>
