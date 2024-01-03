1d1600abb4bf54b74c3faf431e11e73a0764f9a4
Vera Petrosyan <petrosyan.vera.2002@gmail.com>
2024-01-03 15:44:18 +0300

    💄 Change fw for butoon

--- src/components/ButtonBuy.tsx
+++ src/components/ButtonBuy.tsx
@@ -20,7 +20,6 @@ export const ButtonBuy = () => {
         color='black'
         w='100%'
         h='100%'
-        fontWeight={700}
         _hover={{
           backgroundClip: 'text',
           bgGradient: 'linear-gradient(90deg, #B67D3A -3.44%, #FFF 100%)',
